9f54271ee2dc172986cb3fc2c1e07eb6a38d528a
Ayushvik <161769397+Ayushvik@users.noreply.github.com>
2025-01-27 13:26:01 +0530

    GIT PUSH

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,12 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCjR_1NUspIHlZCZD9l5f1DNeYfWLn8PjQ",
+  authDomain: "student-management-db841.firebaseapp.com",
+  projectId: "student-management-db841",
+  storageBucket: "student-management-db841.firebasestorage.app",
+  messagingSenderId: "604053975031",
+  appId: "1:604053975031:web:05aab7a2b4fb6d341a88f2",
+   measurementId: "G-CX6919Q42H"
 };
 
 const app = initializeApp(firebaseConfig);
